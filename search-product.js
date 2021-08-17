f37a30e1d3b3c5496f44955902a3952cb3a3d233
MasudSultan4 <mdsarwarjahanmasudsultan1971@gmail.com>
2021-08-17 06:05:05 +0600

    basic-js-bonus

---
+++ search-product.js
@@ -0,0 +1,8 @@
+const product = [
+    {name: 'samsung' , price: 12000},
+    {name: 'asus laptop', price: 32000},
+    {name: 'apple smart watch', price: 18000},
+    {name: 'binding papar', price: 80},
+    {name: 'lg smart phone', price: 10000},
+    {name: 'old land phone', price: 100}
+]
